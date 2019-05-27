b71bc2ade6fdcf5ffec064a8e44241ef44cebacd
dyuki <unblivion@gmail.com>
2019-05-27 19:24:48 -0300

    I've Seen That Before... (Splash Screen)

--- canIDrive/app/src/main/java/br/com/dyuki/canidrive/LaunchActivity.kt
+++ canIDrive/app/src/main/java/br/com/dyuki/canidrive/LaunchActivity.kt
@@ -1,12 +1,27 @@
 package br.com.dyuki.canidrive
 
+import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
 
 class LaunchActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_launch)
+
+        //Splash Screen But Only for 3 Seconds
+        Handler().postDelayed({
+
+            //Creating Intent
+            val intentMain = Intent(this@LaunchActivity, MainActivity::class.java)
+            //Executing Intent
+            startActivity(intentMain)
+            //Finishing Launcher
+            finish()
+
+        }, 5000)
+
     }
 }
