618463777f001ca4fbfd409415c79130ef4af9d2
dyuki <unblivion@gmail.com>
2019-05-27 21:05:39 -0300

    Main Screen - OK

--- canIDrive/app/src/main/java/br/com/dyuki/canidrive/MainActivity.kt
+++ canIDrive/app/src/main/java/br/com/dyuki/canidrive/MainActivity.kt
@@ -1,12 +1,39 @@
 package br.com.dyuki.canidrive
 
+import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        //Enter Action
+        btnEnter.setOnClickListener {
+
+            //Filtering...
+            if(edtName.text.isNotEmpty()){
+                //Receiving Text
+                val name = edtName.text.toString()
+
+                //Intent => Screen2
+                val intentScreen2 = Intent(this@MainActivity, Screen2Activity::class.java)
+
+                //Load text to intent
+                intentScreen2.putExtra("aName", name)
+
+                //Execute Intent
+                startActivity(intentScreen2)
+
+            }else{
+
+                Toast.makeText(this, "R u Stupid ?", Toast.LENGTH_LONG).show()
+            }
+
+        }
     }
 }
--- canIDrive/app/src/main/java/br/com/dyuki/canidrive/Screen2Activity.kt
+++ canIDrive/app/src/main/java/br/com/dyuki/canidrive/Screen2Activity.kt
@@ -2,11 +2,15 @@ package br.com.dyuki.canidrive
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_screen2.*
 
 class Screen2Activity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_screen2)
+
+        val name = intent.getStringExtra("aName")
+        txvName.text = "Welcome $name"
     }
 }
