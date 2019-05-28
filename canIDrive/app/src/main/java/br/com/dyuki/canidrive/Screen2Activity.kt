c28263147081757cd8ba7883de0c7f7f4e7b2a23
dyuki <unblivion@gmail.com>
2019-05-27 21:40:03 -0300

    Screen2Activity - 50%

--- canIDrive/app/src/main/java/br/com/dyuki/canidrive/Screen2Activity.kt
+++ canIDrive/app/src/main/java/br/com/dyuki/canidrive/Screen2Activity.kt
@@ -2,6 +2,7 @@ package br.com.dyuki.canidrive
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ArrayAdapter
 import kotlinx.android.synthetic.main.activity_screen2.*
 
 class Screen2Activity : AppCompatActivity() {
@@ -10,7 +11,23 @@ class Screen2Activity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_screen2)
 
+        //Retrieve String from MainActivity and Using it
         val name = intent.getStringExtra("aName")
         txvName.text = "Welcome $name"
+
+        //Age Array => (Spinner Content)
+        val ageArray = arrayListOf("Ages:", 1)
+
+        //Creating Spinner Content
+        for(anAge in 2..100){
+            //Adding Content to Array
+            ageArray.add(anAge)
+        }
+
+        //Setting up Adapted Array for Spinner
+        val adaptedArray = ArrayAdapter(this, android.R.layout.simple_spinner_dropdown_item, ageArray)
+
+        //Insert Adapted Array => Spinner
+        spnAges.adapter = adaptedArray
     }
 }
