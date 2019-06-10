bc02ced9f98d3cff6cee46bb9904b60e49f3ac20
dyuki <unblivion@gmail.com>
2019-06-10 19:40:20 -0300

    Camera - OK

--- photoCapture/app/src/main/java/br/com/dyuki/photocapture/MainActivity.kt
+++ photoCapture/app/src/main/java/br/com/dyuki/photocapture/MainActivity.kt
@@ -1,12 +1,38 @@
 package br.com.dyuki.photocapture
 
+import android.app.Activity
+import android.content.Intent
+import android.graphics.Bitmap
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.provider.MediaStore
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+
+    //Request Code
+    val requestCam = 0
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        btnCamera.setOnClickListener {
+            //Camera Intent
+            val callCamera = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
+            //Implicit Intent
+            startActivityForResult(callCamera, requestCam)
+        }
+    }
+
+    //Use Photo Captured
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+
+        if (requestCode == requestCam){
+            if (resultCode == Activity.RESULT_OK && data != null){
+                imgResult.setImageBitmap(data.extras.get("data") as Bitmap)
+            }
+        }
     }
 }
